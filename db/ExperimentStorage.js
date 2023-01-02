e94d2e3e9159bf37ae2b50cd7a1ff9c9d89561bb
Peer <peerfichman@gmail.com>
2023-01-02 18:21:17 +0200

    creating experimentStorage extends MongoStorage and implemnting methods

---
+++ db/ExperimentStorage.js
@@ -0,0 +1,25 @@
+const {MongoStorage} = require ('./MongoStorage');
+
+module.exports = class ExperimentStorage extends  MongoStorage{
+    constructor() {
+        super("experiment");
+    }
+    incVariantSuccessCount(id, variant){
+        return this.update(id, {$inc : {[`variant_success_count.${variant}`]: 1}});
+    }
+    getVariantSuccessCount(id){
+        const experiment = this.retrieve(id);
+        if(!! experiment)
+            return experiment.variant_success_count;
+        else
+            return null;
+    }
+    getCallCount(id){
+        const experiment = this.retrieve(id);
+        if(!! experiment)
+            return experiment.call_count;
+        else
+            return null;
+    }
+
+}
