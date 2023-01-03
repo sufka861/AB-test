07b1d86781ae672ad9d1ce60df4b3bf61cb4e38c
Suf <sufkarmon2@gmail.com>
2023-01-03 20:04:31 +0200

    add controller and routers for Get Put Post Delete ops

--- controller/experimentController.js
+++ controller/experimentController.js
@@ -5,6 +5,39 @@ async function getAllExperiments(req, res) {
     res.send(await experimentDB.find());
 }
 
+async function getExperimentsByID(req, res) {
+    const experimentID = req.params._id;
+    res.send(await experimentDB.retrieve(experimentID));
+}
+
+async function getExperimentsAB(req, res) {
+    res.send(await experimentDB.findGroup("type", "a-b"));
+}
+
+async function getExperimentsFF(req, res) {
+    res.send(await experimentDB.findGroup("type", "f-f"));
+}
+
+async function updateExperimentsByID(req, res) {
+    const experimentID = req.params._id;
+    res.send(await experimentDB.update(experimentID, req.body));
+}
+
+async function deleteExperimentsByID(req, res) {
+    const experimentID = req.params._id;
+    res.send(await experimentDB.delete(experimentID));
+}
+
+async function createExperiments(req, res) {
+    res.send(await experimentDB.create(req.body));
+}
+
 module.exports = {
-    getAllExperiments
+    getAllExperiments,
+    getExperimentsByID,
+    getExperimentsAB,
+    getExperimentsFF,
+    updateExperimentsByID,
+    deleteExperimentsByID,
+    createExperiments,
 }
--- router/experimentRouter.js
+++ router/experimentRouter.js
@@ -3,6 +3,15 @@ const experimentController = require('../controller/experimentController');
 const experimentRouter = new express.Router();
 
 experimentRouter.get('/', experimentController.getAllExperiments);
+experimentRouter.get('/AB', experimentController.getExperimentsAB);
+experimentRouter.get('/:FF', experimentController.getExperimentsFF);
+experimentRouter.get('/:_id', experimentController.getExperimentsByID);
+
+experimentRouter.put('/:_id', experimentController.updateExperimentsByID);
+
+experimentRouter.delete('/:_id', experimentController.deleteExperimentsByID);
+
+experimentRouter.post('/', experimentController.createExperiments);
 
 module.exports = {
     experimentRouter
