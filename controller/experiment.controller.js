80a032b1e0244d43ba54a7fe4a106758c2e946e2
Suf <sufkarmon2@gmail.com>
2023-01-06 15:55:42 +0200

    remove functions from experiment controller

--- controller/experiment.controller.js
+++ controller/experiment.controller.js
@@ -4,14 +4,14 @@ const experimentRepository = new ExperimentRepository();
 
 const getAllExperiments = async (req, res) => {
     const result = await experimentRepository.find();
-    res.status(200).json({ result });
+    res.status(200).json({result});
 }
 
 const getExperimentById = async (req, res) => {
     if (req.params.experiment_id) {
         const experiment_id = req.params.experiment_id;
         const result = await experimentRepository.retrieve(experiment_id);
-        res.status(200).json({ result });
+        res.status(200).json({result});
     }
 }
 
@@ -19,58 +19,46 @@ const getExperimentsByAccountId = async (req, res) => {
     if (req.params.account_id) {
         const account_id = req.params.account_id;
         const experiments = await experimentRepository.findByAccount("account_id", account_id);
-        res.status(200).json( {experiments});
+        res.status(200).json({experiments});
     }
 }
 
 const getExperimentsAB = async (req, res) => {
     const account_id = req.params.account_id;
     const result = await experimentRepository.findGroup("type", "a-b", "account_id", account_id);
-    res.status(200).json({ result });
+    res.status(200).json({result});
 }
 
 const getExperimentsFF = async (req, res) => {
     const account_id = req.params.account_id;
     const result = await experimentRepository.findGroup("type", "f-f", "account_id", account_id);
-    res.status(200).json({ result });
-}
-
-const getVariantByExperimentId = async (req, res) => {
-    res.sendStatus(200);
-    res.send();
+    res.status(200).json({result});
 }
 
 const getExperimentsByDate = async (req, res) => {
     const year = req.query.year;
     const month = req.query.month;
     const result = await experimentRepository.findByDate(year, month);
-    res.status(200).json({ result });
+    res.status(200).json({result});
 }
 
 const createExperiments = async (req, res) => {
     const result = await experimentRepository.create(req.body)
-    res.status(200).json({ result });
+    res.status(200).json({result});
 }
 
 const updateExperimentsByID = async (req, res) => {
     const experimentID = req.params.experiment_id;
     const result = await experimentRepository.update(experimentID, req.body)
-    res.status(200).json({ result });
+    res.status(200).json({result});
 }
 
 const deleteExperimentsByID = async (req, res) => {
     const experimentID = req.params.experiment_id;
     const result = await experimentRepository.delete(experimentID)
-    res.status(200).json({ result });
+    res.status(200).json({result});
 }
 
-const runExperiment = async (req, res) => {
-
-}
-
-const declareGoal = async (req, res) => {
-
-}
 
 
 module.exports = {
@@ -78,12 +66,9 @@ module.exports = {
     getExperimentById,
     getExperimentsByAccountId,
     getExperimentsAB,
-    getExperimentsFF,
-    getVariantByExperimentId,
+    getExperimentsFF,,
     getExperimentsByDate,
     updateExperimentsByID,
     deleteExperimentsByID,
     createExperiments,
-    runExperiment,
-    declareGoal,
 }
