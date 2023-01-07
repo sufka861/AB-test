043b5d119674a64a043545b65659cab6c78b2c36
Suf <sufkarmon2@gmail.com>
2023-01-07 21:51:40 +0200

    cron job complete still bad query on date

--- middleware/cron.job.js
+++ middleware/cron.job.js
@@ -1,49 +1,23 @@
 const cron = require("node-cron");
-const Util = require("../Service/utils");
 const ExperimentRepository = require("../repositories/experiment.repository");
 
-const experimentStatusUpdate = cron.schedule("* * * * *", async () => {
-    const now = new Date().toISOString();
-    const query = {status: "planned", duration : { start_time: {$gte: now}}}
-    const experiments = await ExperimentRepository.findByQuery(query);
+const experimentStatusUpdate = cron.schedule("* * * * * *", async () => {
+    const now = new Date();
+    let query = {status: "planned", duration: {start_time:{$lte: now}, end_time: {$gte: now}}};
+    let experiments = await ExperimentRepository.findByQuery(query);
     console.log(experiments);
+    experiments.forEach((experiment) => {
+        ExperimentRepository.update(experiment._id, {status: "active"})
+    });
 
+    query = {status: "active", duration: {end_time:{$gte: now}, start_time: {$lte: now}}};
+    experiments = await ExperimentRepository.findByQuery(query);
+    experiments.forEach((experiment) => {
+        ExperimentRepository.update(experiment._id, {status: "ended"})
+    });
+    console.log(experiments);
 })
-
-
 
 module.exports = {
     experimentStatusUpdate,
 }
-
-//
-// const experimentActiveAndEndCronJobs = (experiment)=> {
-//     //startTime&endTime - Time in UTC!
-//     const {start_time, end_time} = experiment;
-//     const diffTime = end_time.getDate() - start_time.getDate()
-//
-//     // cron.schedule(start_time, async () => {
-//     //     await ExperimentStorage.updateExperimentStatus(experiment._id, "active");
-//     // })
-//     // cron.schedule(end_time, async () => {
-//     //     await ExperimentStorage.updateExperimentStatus(experiment._id, "ended");
-//     // })
-//
-//     const task = cron.schedule(start_time, () => {
-//
-//         const intervalObj = setInterval(() => {
-//             exp.status = "active";
-//             if (Util.checkIfTerminated() == true) {
-//                 clearInterval(intervalObj);
-//             }
-//         }, diffTime);
-//         clearInterval(intervalObj);
-//     });
-//
-//     exp.status = "Ended";
-//     task.stop();
-// }
-//
-// module.exports = {
-//     experimentActiveAndEndCronJobs
-// }
