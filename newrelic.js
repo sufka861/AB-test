8e6ed114375fcd21803c2fc437e270b84ca7691d
Suf <sufkarmon2@gmail.com>
2023-01-08 02:13:02 +0200

    change

--- newrelic.js
+++ newrelic.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
-* New Relic agent configuration.
+* New Relic agent configuration
 *
 * See lib/config/default.js in the agent distribution for a more complete
 * description of configuration variables and their potential values.
