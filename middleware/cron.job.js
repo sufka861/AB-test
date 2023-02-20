8d2b6b1acf5b1437a963695cbdf7495ef7ee8d7d
mohamadaboria <mohamadaboria116@gmail.com>
2023-02-21 00:29:13 +0200

    "fixed node crone function"

--- middleware/cron.job.js
+++ middleware/cron.job.js
@@ -1,20 +1,39 @@
 const cron = require("node-cron");
 const ExperimentRepository = require("../repositories/experiment.repository");
 
-const experimentStatusUpdate = cron.schedule("* 0 * * *", async () => {
-    const now = new Date();
-    let query = {status: "planned", duration: {start_time:{$lte: now}, end_time: {$gte: now}}};
-    let experiments = await ExperimentRepository.findByQuery(query);
-    experiments.forEach((experiment) => {
-        ExperimentRepository.update(experiment._id, {status: "active"})
-    });
+const experimentStatusUpdate =(startTime, endTime) => {
+    cron.schedule("* * * * *", async () => {
+        const currentTime = new Date();
+        const start = new Date(currentTime);
+        const end = new Date(currentTime);
+        start.setHours(...startTime.split(':'));
+        end.setHours(...endTime.split(':'));
 
-    query = {status: "active", duration: {end_time:{$gte: now}, start_time: {$lte: now}}};
-    experiments = await ExperimentRepository.findByQuery(query);
-    experiments.forEach((experiment) => {
-        ExperimentRepository.update(experiment._id, {status: "ended"})
-    });
-})
+        if (currentTime >= start && currentTime <= end) {
+            console.log('active');
+            const now = new Date();
+            let query = {status: "planned", duration: {start_time:{$lte: now}, end_time: {$gte: now}}};
+            let experiments = await ExperimentRepository.findByQuery(query);
+            experiments.forEach((experiment) => {
+                ExperimentRepository.update(experiment._id, {status: "active"})
+            });
+
+          }
+          // Check if the current time is after the time interval
+          else if (currentTime > end) {
+            console.log('ended')
+            query = {status: "active", duration: {end_time:{$gte: now}, start_time: {$lte: now}}};
+            experiments = await ExperimentRepository.findByQuery(query);
+            experiments.forEach((experiment) => {
+                ExperimentRepository.update(experiment._id, {status: "ended"})
+            });
+          }
+
+
+    })
+}
+
+
 
 module.exports = {
     experimentStatusUpdate,
