1cddd3bc2bf3d847127c5ab32a2227a8a43ecc12
Peer <peerfichman@gmail.com>
2023-02-23 15:50:45 +0200

    removing spaces

--- controller/experiment.creation.controller.js
+++ controller/experiment.creation.controller.js
@@ -7,26 +7,18 @@ const {bodyValidator} = require("../validators/body.validator");
 const {isLogForwardingEnabled} = require("newrelic/lib/util/application-logging");
 const {log} = require("winston");
 
-
 const createExperimentWithGoals = async (req, res) => {
     if (!bodyValidator(req)) throw new BodyNotSent();
     const {experiment, goals} = req.body;
-
     if (!experiment || !goals) throw new PropertyNotFound("Invalid request body for creating new experiment");
-
-
     experiment.goals = await GoalRepository.createMany(goals)
         .then(newGoals => newGoals.map(newGoal => newGoal._id))
         .catch(err => {
             if (err) throw new ServerUnableError(err.message)
         })
-
     const newExperiment = await ExperimentRepository.create(experiment);
-
     if (!newExperiment) throw ServerUnableError("Creating new experiment");
-
     res.status(200).send(newExperiment);
-
 }
 
 module.exports = {createExperimentWithGoals};
