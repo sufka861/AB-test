54d82042e3e6b204ce2477adf0e39ff361e07c58
shir149 <shiramar0401@gmail.com>
2023-02-25 10:31:02 +0200

    update tests to featurelogice

--- test/service.test/feature.logic.test.js
+++ test/service.test/feature.logic.test.js
@@ -6,9 +6,16 @@ describe('featureCheckAttributes', () => {
 
     it('should return the ON variant', () => {
         const endUserReq = {};
-        const experiment = { variantsFF: { ON: { foo: 'bar' }, OFF: { foo: 'baz' } } };
+        const experiment = { variantsFF: { ON: { foo: true }, OFF: { foo: false } } };
         const result = featureCheckAttributes(endUserReq, experiment);
-        expect(result).to.deep.equal({ ON: { foo: 'bar' } });
+        expect(result).to.deep.equal({ ON: { foo: true } });
+    });
+
+    it('should return the OFF variant', () => {
+        const endUserReq = {};
+        const experiment = { variantsFF: { ON: { foo: false }, OFF: { foo: true } } };
+        const result = featureCheckAttributes(endUserReq, experiment);
+        expect(result).to.deep.equal({ ON: { foo: false } });
     });
 
 });
