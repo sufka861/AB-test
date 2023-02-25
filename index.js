89996980197618ae054ef990b79ffa32875485fb
Peer <peerfichman@gmail.com>
2023-02-25 11:31:26 +0200

    working function inAtrributeReqCount

--- index.js
+++ index.js
@@ -9,30 +9,33 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const index = express();
 const requestIp = require("request-ip");
-const { errorHandler } = require("./middleware/errorHandler.mw");
+const {errorHandler} = require("./middleware/errorHandler.mw");
 const logPath = path.join(__dirname, "logs", "http.log");
 const port = process.env.PORT || 3000;
-const { experimentRouter } = require("./router/experiment.router");
-const { testRouter } = require("./router/external.routes");
-const { userRouter } = require("./router/user.routes");
+const {experimentRouter} = require("./router/experiment.router");
+const {testRouter} = require("./router/external.routes");
+const {userRouter} = require("./router/user.routes");
 const statsRouter = require("./router/stats.router");
 const {goalRouter} = require("./router/goal.router");
-const { experimentStatusUpdate } = require("./Service/cron.job");
+const {experimentStatusUpdate} = require("./Service/cron.job");
+
+const ExperimentRepository = require('./repositories/experiment.repository')
+const {log} = require("winston");
 
 index.use(express.json());
-index.use(express.urlencoded({ extended: true }));
+index.use(express.urlencoded({extended: true}));
 index.use(
-  logger(":date --> :method :url :status :response-time ms", {
-    stream: fs.createWriteStream(logPath, { flags: "a" }),
-  })
+    logger(":date --> :method :url :status :response-time ms", {
+        stream: fs.createWriteStream(logPath, {flags: "a"}),
+    })
 );
 index.use(cookieParser());
 
 index.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
 );
 
 // Routes goes here!
@@ -44,6 +47,7 @@ index.use("/goal", goalRouter);
 index.use("/stats", statsRouter);
 index.use(errorHandler);
 
+
 index.listen(port, () => {
-  console.log(`Server is listening on port ${port}...`);
+    console.log(`Server is listening on port ${port}...`);
 });
