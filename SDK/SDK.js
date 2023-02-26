648dd84fc4d099f0f46a48f4549c63e226a71e45
Peer <peerfichman@gmail.com>
2023-02-26 14:39:43 +0200

    module to export as SDK

---
+++ SDK/SDK.js
@@ -0,0 +1,30 @@
+const requestIp = require('request-ip');
+const geoip = require("geoip-lite");
+const parser = require("ua-parser-js");
+
+const getIp = (endUserReq) =>{
+    return requestIp.getClientIp(endUserReq)
+}
+
+const getLocationCode = (endUserReq) =>{
+    return geoip.lookup(getIp(endUserReq));
+}
+
+const getUserBrowserDevice = (endUserReq) => {
+    const userAgentInfo = parser(endUserReq.headers["user-agent"]);
+    return  {
+        browser: userAgentInfo.browser.name,
+        device: userAgentInfo.device.type || "desktop",
+    };
+}
+
+const filterUserAttributes = (endUserReq, endUserCustomAttributes) =>{
+    return {
+        testAttributes:{
+            location : getLocationCode(endUserReq),
+            ...getUserBrowserDevice(endUserReq)
+        },
+        customAttributes: endUserCustomAttributes
+
+    }
+}
