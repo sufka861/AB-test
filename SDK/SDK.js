c325d6ce4919b1c0ade84f7bbc100a0de1c65050
Peer <peerfichman@gmail.com>
2023-02-26 16:51:59 +0200

    added uuid to user

--- SDK/SDK.js
+++ SDK/SDK.js
@@ -2,29 +2,29 @@ const requestIp = require('request-ip');
 const geoip = require("geoip-lite");
 const parser = require("ua-parser-js");
 
-const getIp = (endUserReq) =>{
+const getIp = (endUserReq) => {
     return requestIp.getClientIp(endUserReq)
 }
 
-const getLocationCode = (endUserReq) =>{
+const getLocationCode = (endUserReq) => {
     return geoip.lookup(getIp(endUserReq));
 }
 
 const getUserBrowserDevice = (endUserReq) => {
     const userAgentInfo = parser(endUserReq.headers["user-agent"]);
-    return  {
+    return {
         browser: userAgentInfo.browser.name,
         device: userAgentInfo.device.type || "desktop",
     };
 }
 
-const filterUserAttributes = (endUserReq, endUserCustomAttributes) =>{
+const filterUserAttributes = (endUserReq, endUserCustomAttributes) => {
     return {
-        testAttributes:{
-            location : getLocationCode(endUserReq),
+        testAttributes: {
+            location: getLocationCode(endUserReq),
             ...getUserBrowserDevice(endUserReq)
         },
-        customAttributes: endUserCustomAttributes
-
+        customAttributes: endUserCustomAttributes,
+        uuid:  endUserReq.cookies.uuid || null
     }
 }
